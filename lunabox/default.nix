8e3e576588c0140732582b25b64738c7c48030d4
Luna <Luna>
2025-04-04 15:04:57 +0200

    meow

--- lunabox/default.nix
+++ lunabox/default.nix
@@ -11,6 +11,7 @@
     extraGroups  = [ "wheel" "networkmanager" ];
     openssh.authorizedKeys.keys  = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjendyGMtskVQKga3Q5ms+/9oXYMM9v/jpMl+zn1Vj0 luna@bar" ];
   };
+  boot.loader.systemd-boot.enable = true;
 
 
 }
