696de2be1a088f33e89e2e3cb211617f79a9e0a8
Luna <Luna>
2025-04-04 15:39:34 +0200

    meow

--- lunabox/default.nix
+++ lunabox/default.nix
@@ -14,4 +14,15 @@
   boot.loader.systemd-boot.enable = true;
   boot.initrd.systemd.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  services.openssh = {
+  enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
  }
