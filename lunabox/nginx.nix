23fd20299e6aff057a1a2e8cfdeeea932a6cdaa6
Luna <Luna>
2025-04-06 21:28:08 +0200

    meow

--- lunabox/nginx.nix
+++ lunabox/nginx.nix
@@ -5,9 +5,20 @@
     enable = true;
 
     clientMaxBodySize = "16m";
+    recommendedProxySettings = true;
     recommendedTlsSettings = true;
     recommendedOptimisation = true;
     recommendedGzipSettings = true;
+    virtualHosts = {
+      "jellyfin.estrogen.today" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:8096";
+        };
+      };
+    };
+
   };
   security.acme = {
     acceptTerms = true;
