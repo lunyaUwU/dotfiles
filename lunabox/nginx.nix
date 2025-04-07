301293419c7308de18a9f18c741e48c718ff41bb
Luna <Luna>
2025-04-07 14:28:35 +0200

    schrimp

--- lunabox/nginx.nix
+++ lunabox/nginx.nix
@@ -24,7 +24,15 @@
         locations."/" = {
           proxyPass = "http://127.0.0.1:8080";
         };
-    };
+      };
+      "shrimp.estrogen.today" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3000";
+        };
+      };
+
 
     };
   };
