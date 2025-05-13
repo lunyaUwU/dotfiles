d91baae220f90688b6411d2ef9affc50f7448b21
Luna <Luna>
2025-05-13 14:06:47 +0200

    meow

--- lunabox/nextcloud.nix
+++ lunabox/nextcloud.nix
@@ -3,12 +3,20 @@
   services.nextcloud = {
     enable = true;
     hostName = "cloud.estrogen.today"; 
+    database.createLocally = true;
+    configureRedis = true;
+    maxUploadSize = "32G";
+    https = true;
+    enableBrokenCiphersForSSE = false;
+
     config = { 
+      adminuser = "admin";
       adminpassFile = "/var/nextcloud-pw";
       dbtype = "pgsql";
     };
   };
   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+
     forceSSL = true;
     enableACME = true;
   };
