aec4eb78be6208c8735e795db7ed031d2c9028a4
Luna <Luna>
2025-07-08 21:47:30 +0200

    meow

--- lunabox/grafana.nix
+++ lunabox/grafana.nix
@@ -44,7 +44,7 @@
     };
     scrapeConfigs = [
       {
-        job_name = "chrysalis";
+        job_name = "prometheus";
         static_configs = [{
           targets = [ 
           "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" 
