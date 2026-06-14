4e8eaef26e2ea8e96eb3bbf76053da56efee57a7
Luna <73499216+lunyaUwU@users.noreply.github.com>
2026-06-15 00:19:49 +0200

    Update default.nix

--- lunabox/default.nix
+++ lunabox/default.nix
@@ -15,7 +15,7 @@
 #   ./jellyfin.nix
 #    ./gotosocial.nix
 #    ./nextcloud.nix
-    ./grafana.nix
+#   ./grafana.nix
   ];
   system.stateVersion = "25.05";  
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
