74b90947f55b20bd11acb96c6b0c2b5ec4626055
Luna Arson <luna@Lunas-MacBook-Air.local>
2026-08-01 16:44:27 +0200

    meow

---
+++ lunabox/keycloak.nix
@@ -0,0 +1,21 @@
+{...}:
+{
+  services.keycloak = {
+    enable = true;
+
+    database = {
+      type = "postgresql";
+      createLocally = true;
+
+      username = "keycloak";
+      passwordFile = "/etc/nixos/secrets/keycloak_psql_pass";
+    };
+
+    settings = {
+      hostname = "keycloak.estrogen.today";
+      http-enabled = true;
+      http-port = 38080;
+      hostname-strict-https = true;
+    };
+  };
+}
